69a87b259b25341783402451a86b56c244483ae3
root <root@ip-172-26-13-147.ca-central-1.compute.internal>
2019-03-26 22:16:07 +0000

    Provide full path to client_secrets.json

--- catalog.py
+++ catalog.py
@@ -729,7 +729,7 @@ def glogin():
 
     try:
         # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow = flow_from_clientsecrets('/var/www/catalog/client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
